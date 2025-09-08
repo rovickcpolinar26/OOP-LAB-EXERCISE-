c514db9cae12a1fe2ef1856199b0f4f4c33dac44
ROVICK POLINAR _2E <rovickcesar@gmail.com>
2025-09-08 21:34:34 +0800

    Add files via upload

---
+++ Exer2_PolinarRovick/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    Phone phone1 = new Phone("Samsung", "Galaxy S23", 128, 15.167);
+    Phone phone2 = new Phone("Apple", "iPhone 15", 256, 34.578);
+
+        phone1.displayPhoneDetails();
+        phone2.displayPhoneDetails();
+
+    }
+}
